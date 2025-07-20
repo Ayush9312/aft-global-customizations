7b9126b8c700ed9e40e73b2c296af5b0178e8d92
moneesh <moneesh.reddy@tmdc.io>
2025-07-20 12:49:39 +0530

    global

--- terraform/s3-block-public.tf
+++ terraform/s3-block-public.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_account_public_access_block" "global-block" {
+resource "aws_s3_account_public_access_block" "global-blocks" {
   block_public_acls       = true
   block_public_policy     = true
 }
