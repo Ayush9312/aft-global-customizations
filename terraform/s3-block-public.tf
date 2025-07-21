0d59f1d525f2b2a68161aad454fe681c3ec0e2b6
AYUSH PANDEY <ayushp@umbrellainfocare.com>
2025-07-21 10:30:33 +0530

    code changes

--- terraform/s3-block-public.tf
+++ terraform/s3-block-public.tf
@@ -2,3 +2,8 @@ resource "aws_s3_account_public_access_block" "global-blocks" {
   block_public_acls       = true
   block_public_policy     = true
 }
+
+
+
+
+### going to step functions and executing global customization on console ##
